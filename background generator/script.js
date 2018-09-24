efaa56a8fbe364f8491531eaed3531f98d0de8be
Leonardo Aranguren <leoaranguren10@gmail.com>
2018-09-24 00:10:56 -0400

    Updating Website

---
+++ background generator/script.js
@@ -0,0 +1,42 @@
+let body = document.getElementById("cuerpo"); 
+let color1 = document.querySelector(".color1"); 
+let color2 = document.querySelector(".color2"); 
+let css = document.querySelector("h3");
+let random = document.querySelector(".random");
+let radiusButtom = document.getElementById("radiusButtom");
+
+color1.addEventListener("input", setGradient);
+color2.addEventListener("input", setGradient);
+random.addEventListener("click", setRandColor);
+
+color1.value = "#ff0000";
+color2.value = "#00ffff";
+
+setGradient();
+    
+function setGradient() {
+    
+    body.style.background = 
+    "linear-gradient(to right, "
+    + color1.value
+    +", "
+    + color2.value
+    + ")";
+
+    css.textContent = body.style.background + ";";
+    sameColor();
+}
+
+function setRandColor() {
+	color1.value = "#"+(Math.random()*0xFFFFFF<<0).toString(16); 
+	color2.value = "#"+(Math.random()*0xFFFFFF<<0).toString(16);
+	setGradient(); 
+}
+
+function sameColor (){
+   color1.style.backgroundColor = color1.value;
+   color2.style.backgroundColor = color2.value;
+}
+
+
+ 
