d717696f63b64490f8757430c8327fade199612b
Leonardo <leoaranguren10@gmail.com>
2019-03-04 23:20:31 -0400

    change script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let body = document.getElementById("cuerpo"); 
+let body = document.querySelector("body"); 
 let color1 = document.querySelector(".color1"); 
 let color2 = document.querySelector(".color2"); 
 let css = document.querySelector("h3");
